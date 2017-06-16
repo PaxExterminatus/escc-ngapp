5b80054859262a00d9b3f0caa39e3964bf04faee
Pax <paxexterminatus@gmail.com>
2017-06-16 12:26:14 +0300

    Comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'; // Подключаем Reactive Forms
 
 import { AppComponent } from './app.component';
-import { AppOrderComponent } from './order-app/order'; // Приложенение формы
+import { AppOrderComponent } from './order-app/order'; // Приложение формы
 
 import { OrderFormComponent } from './order-cmp/order-form'; // Компонент формы
 
 @NgModule({
   declarations: [
     AppComponent,      // Корневое приложение
-    AppOrderComponent, // Приложенение формы
+    AppOrderComponent, // Приложение формы
     OrderFormComponent // Компонент формы
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule // Подключаем Reactive Forms
   ],
   providers: [],
   bootstrap: [AppComponent, AppOrderComponent] // Делаем селектор приложения доступным из внешних файлов (index.html)
